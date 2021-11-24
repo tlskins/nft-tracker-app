5f482656b2a6c1ca3944285d6dd5d5cb98facee5
Tim Lee <tlee87@gmail.com>
2021-11-24 13:28:56 -0500

    redeploy

--- components/SendPaymentToTreasury.tsx
+++ components/SendPaymentToTreasury.tsx
@@ -79,7 +79,7 @@ const SendPaymentToTreasury: FC = () => {
       }
     } catch( err ) {
       console.log('transaction err ', err)
-      toast.success(`Error processing transaction in the Solana network: ${err}`, {
+      toast.error(`Error processing transaction in the Solana network: ${err}`, {
         position: toast.POSITION.TOP_CENTER
       })
     }
