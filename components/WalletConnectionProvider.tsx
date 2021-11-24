b111849dbb1abe2ab5e4129bb65384b18dee8a27
Tim Lee <tlee87@gmail.com>
2021-11-24 13:19:14 -0500

    enable mainnet via env

--- components/WalletConnectionProvider.tsx
+++ components/WalletConnectionProvider.tsx
@@ -14,7 +14,10 @@ import { FC, ReactNode, useMemo } from 'react'
 
 export const WalletConnectionProvider: FC<{ children: ReactNode }> = ({ children }) => {
   // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Devnet
+  const network = process.env.NEXT_PUBLIC_SOLANA_NETWORK === 'mainnet-beta' ?
+    WalletAdapterNetwork.Mainnet
+    :
+    WalletAdapterNetwork.Devnet
 
   // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl( network ), [ network ])
